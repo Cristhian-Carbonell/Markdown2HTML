4e2f13b67074b7bdc42e9814d10c7a2c57505174
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-05-08 18:40:47 -0500

    Headings

--- markdown2html.py
+++ markdown2html.py
@@ -17,6 +17,7 @@ from sys import argv, exit, stderr
 import re
 from os.path import exists
 
+
 def expresion(read_line, menssage):
     temp = "(0)+".replace("0", menssage)
     expr = re.match(temp, read_line)
@@ -24,12 +25,28 @@ def expresion(read_line, menssage):
         return str(expr.group())
     return None
 
+
 def heading(expr1, value1, read_line1):
     level = str(len(expr1))
     op_value = value1[0].replace("0", level)
     cl_value = value1[1].replace("0", level)
     cont = "" + read_line1.replace(expr1 + " ", "")
     return op_value + cont + cl_value + "\n"
+
+
+def init(openlist, menssag):
+    text1 = ""
+    if openlist:
+        text1 = menssag[0] + "\n"
+    else:
+        text1 = menssag[1] + "\n"
+    return text1
+
+
+def add_item(items, read_line):
+    text = read_line.replace("\n", "")
+    text = text.replace("- ", "")
+    return item[0] + text + item[1] + "\n"
 
 if __name__ == "__main__":
 
@@ -45,7 +62,11 @@ if __name__ == "__main__":
         exit(1)
 
     file2 = argv[2]
-    menssage = {"#": ["<h0>", "</h0>"]}
+    menssage = {"#": ["<h0>", "</h0>"],
+                "-": ["<ul>", "</ul>"]}
+    item = ["<li>", "</li>"]
+    lists = {"-": False, "*": False}
+    openlist = [False, ""]
     word = ''
     expr = None
 
@@ -54,9 +75,24 @@ if __name__ == "__main__":
             for key, value in menssage.items():
                 expr = expresion(read_line, key)
                 if expr is None:
-                    word += read_line
+                    # word += read_line
                     continue
-                word += heading(expr, value, read_line.replace("\n", ""))
+
+                if expr in lists:
+                    if openlist[0] is False:
+                        openlist[0] = True
+                        openlist[1] = expr
+                        word += init(openlist[0], menssage[openlist[1]])
+                    word += add_item(item, read_line)
+                else:
+                    if openlist[0]:
+                        openlist[0] = False
+                        word += init(openlist[0], menssage[openlist[1]])
+
+                    word += heading(expr, value, read_line.replace("\n", ""))
+        else:
+            if openlist[0]:
+                word += init(openlist[0], menssage[openlist[1]])
 
     with open(file2, "w") as new_f:
         new_f.write(word)
